fea04b64d1174a1b45cac1aec85207ce02e067e3
Asad <asadalpha77@gmail.com>
2025-03-01 23:43:30 +0530

    first commit

---
+++ config/env.js
@@ -0,0 +1,8 @@
+import { config } from "dotenv";
+
+
+const envFile = `.env.${process.env.NODE_ENV || 'development'}.local`;
+
+config({ path: envFile });
+
+export const { PORT, NODE_ENV } = process.env;
---
+++ routes/subscription.routes.js
@@ -0,0 +1,38 @@
+import e, { Router } from "express";
+
+const subscriptionRouter = Router();
+
+// GET /subscriptions -> get all subscriptions
+
+subscriptionRouter.get("/", (req, res) => {
+  res.send({ title: "GET ALL SUBSCRIPTIONS" });
+});
+
+subscriptionRouter.get("/:id", (req, res) => {
+  res.send({ title: "GET subscription details" });
+});
+
+subscriptionRouter.post("/", (req, res) => {
+  res.send({ title: "CREATE subscription" });
+});
+
+subscriptionRouter.put("/:id", (req, res) => {
+  res.send({ title: "UPDATE subscription" });
+});
+
+subscriptionRouter.delete("/:id", (req, res) => {
+  res.send({ title: "DELETE subscription" });
+});
+
+subscriptionRouter.get("/user/:id", (req, res) => {
+  res.send({ title: "GET all user subscription" });
+});
+
+subscriptionRouter.put("/:id/cancle", (req, res) => {
+  res.send({ title: "CANCLE subscription" });
+});
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+  res.send({ title: "CANCLE subscription" });
+});
+
+export default subscriptionRouter;
